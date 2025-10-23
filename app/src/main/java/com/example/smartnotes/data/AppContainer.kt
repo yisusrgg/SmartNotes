6025494d3e5b1009cec23cd3b13c807a99bec6b7
lizetglm <lizetglm@gmail.com>
2025-10-22 18:55:24 -0600

    App container (crea instacias de los repositorios y BD)

---
+++ app/src/main/java/com/example/smartnotes/data/AppContainer.kt
@@ -0,0 +1,50 @@
+package com.example.smartnotes.data
+
+import android.content.Context
+import com.example.smartnotes.data.database.SmartNotesDatabase
+import com.example.smartnotes.data.repository.ArchivosRepository
+import com.example.smartnotes.data.repository.NotasTareasRepository
+import com.example.smartnotes.data.repository.OfflineArchivosRepository
+import com.example.smartnotes.data.repository.OfflineNotasTareasRepository
+import com.example.smartnotes.data.repository.OfflineRecordatoriosRepository
+import com.example.smartnotes.data.repository.RecordatoriosRepository
+
+interface AppContainer {
+    val notasTareasRepository: NotasTareasRepository
+    val archivosRepository: ArchivosRepository
+    val recordatoriosRepository: RecordatoriosRepository
+}
+
+/**
+ * Implementación concreta del contenedor de dependencias.
+ * Crea las instancias de base de datos y repositorios.
+ */
+class AppDataContainer(private val context: Context) : AppContainer {
+    //EJEMPLO ANTERIOR
+    //override val notasTareasRepository: NotasTareasRepository by lazy {
+    //    OfflineNotasTareasRepository(
+    //        SmartNotesDatabase.getDatabase(context).itemDao())
+    //}
+
+    // Base de datos
+    private val database by lazy {
+        SmartNotesDatabase.getDatabase(context)
+    }
+
+    // Repositorio de Notas y Tareas
+    override val notasTareasRepository: NotasTareasRepository by lazy {
+        OfflineNotasTareasRepository(
+            database.notasTareasDao()
+        )
+    }
+
+    // Repositorio de Archivos Adjuntos
+    override val archivosRepository: ArchivosRepository by lazy {
+        OfflineArchivosRepository(database.archivosDao())
+    }
+
+    // Repositorio de Recordatorios
+    override val recordatoriosRepository: RecordatoriosRepository by lazy {
+        OfflineRecordatoriosRepository(database.recordatoriosDao())
+    }
+}
